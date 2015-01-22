f0d1ec5e37c75e7a8c6b3b700fa19d19b19f0be8
Sergey Panfilov <sergray@gmail.com>
2015-01-22 18:40:07 +0300

    Handle submission of the word

--- english_words_game/words/tests.py
+++ english_words_game/words/tests.py
@@ -46,3 +46,7 @@ class TestWordSubmissionView(TestCase):
     def test_get(self):
         response = self.client.get(self.test_url)
         self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        response = self.client.post(self.test_url, data={'english_word': 'Netherlands'})
+        self.assertEqual(response.status_code, 200)
