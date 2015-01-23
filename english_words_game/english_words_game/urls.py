d497458a017af93e34061c0965bab6fa5d5a32f6
Sergey Panfilov <sergray@gmail.com>
2015-01-23 10:00:18 +0300

    Rearrange urls. Show top scores first

--- english_words_game/english_words_game/urls.py
+++ english_words_game/english_words_game/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url('^$', 'words.views.word_submission', name='word-submission'),
-    url(r'^top_scores/$', 'scores.views.top_scores_list', name='top-scores'),
+    url(r'^$', 'scores.views.top_scores_list', name='top-scores'),
+    url('^submit/$', 'words.views.word_submission', name='word-submission'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
         {'extra_context': {'next': '/'}}, name='login'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
