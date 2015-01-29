e24b839876c408c84b22ab7e4ef81965b8fd6f26
Sergey Panfilov <sergray@gmail.com>
2015-01-29 13:27:58 +0300

    Do not silently pass import failure to avoid runtime surprizes

--- english_words_game/words/models.py
+++ english_words_game/words/models.py
@@ -5,10 +5,7 @@ from django.db import models
 logger = logging.getLogger(__file__)
 
 # hack to trigger loading of corpora at application start
-try:
-    from nltk.corpus import wordnet
-    logger.debug('Loading NLTK wordnet')
-    getattr(wordnet, 'synsets')
-    logger.debug('Loaded NLTK wordnet')
-except ImportError:
-    pass
+from nltk.corpus import wordnet
+logger.debug('Loading NLTK wordnet')
+getattr(wordnet, 'synsets')
+logger.debug('Loaded NLTK wordnet')
